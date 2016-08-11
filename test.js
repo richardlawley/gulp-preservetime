e78c27620756ebf4d7a41e4b59891337ea66ab3c
Andrea Baron <andrea@bhweb.it>
2016-08-11 17:34:49 +0200

    Fix tests
    The atime check was failing because the atime of the source files was
    updated when the first test copied the files; by recreating the test
    environment for each test the problem is solved

--- test.js
+++ test.js
@@ -10,21 +10,17 @@ var atime = new Date(2000, 1, 2, 3, 4, 5),
 	mtime = new Date(2010, 10, 12, 13, 14, 15),
 	srcdir = './test-src',
 	dest = './test-dest';
-var	subdir = srcdir + '/subdir'; 
+var	subdir = srcdir + '/subdir';
 var firstTestFile = '/a',
 	secondTestFile = '/subdir/b';
 
-before(function() {
+beforeEach(function() {
 	clearUp();
 	fs.mkdirsSync(subdir);
 	fs.writeFileSync(srcdir + firstTestFile, 'Test Data');
 	fs.utimesSync(srcdir + firstTestFile, atime, mtime);
 	fs.writeFileSync(srcdir + secondTestFile, 'Test Data');
 	fs.utimesSync(srcdir + secondTestFile, atime, mtime);
-});
-
-beforeEach(function() {
-	clearOutput();
 })
 
 after(function() {
@@ -32,47 +28,47 @@ after(function() {
 })
 
 it('should preserve file mtime', function(cb) {
-	
+
 	gulp.src(srcdir + '/**/*')
 		.pipe(gulp.dest(dest))
 		.pipe(preservetime())
 		.pipe(es.wait(function(err, data) {
-			
+
 			// Read file modification times in dest
 			var firstFileStats = fs.statSync(dest + firstTestFile),
 				secondFileStats = fs.statSync(dest + secondTestFile);
-			
+
 			firstFileStats.mtime.getTime().should.equal(mtime.getTime());
 			secondFileStats.mtime.getTime().should.equal(mtime.getTime());
-			
+
 			cb();
 		}));
 });
 
 it('should preserve file atime', function(cb) {
-	
+
 	gulp.src(srcdir + '/**/*')
 		.pipe(gulp.dest(dest))
 		.pipe(preservetime())
 		.pipe(es.wait(function(err, data) {
-			
+
 			// Read file modification times in dest
 			var firstFileStats = fs.statSync(dest + firstTestFile),
 				secondFileStats = fs.statSync(dest + secondTestFile);
-			
+
 			firstFileStats.atime.getTime().should.equal(atime.getTime());
 			secondFileStats.atime.getTime().should.equal(atime.getTime());
-			
+
 			cb();
 		}));
 });
 
 
 function clearUp() {
-	if (fs.existsSync(srcdir)) { 
+	if (fs.existsSync(srcdir)) {
 		fs.removeSync(srcdir);
 	}
-	clearOutput(); 
+	clearOutput();
 }
 
 function clearOutput() {

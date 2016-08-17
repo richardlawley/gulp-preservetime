a597a3cfaba1fc6561403cb631110a422464b550
iBobo <andrea@bhweb.it>
2016-08-17 11:05:13 +0200

    Changed to use futimes instead of utimes (#1)
    Changed to use futimes instead of utimes to give microsecond precision

--- test.js
+++ test.js
@@ -10,21 +10,21 @@ var atime = new Date(2000, 1, 2, 3, 4, 5),
 	mtime = new Date(2010, 10, 12, 13, 14, 15),
 	srcdir = './test-src',
 	dest = './test-dest';
-var	subdir = srcdir + '/subdir'; 
+var	subdir = srcdir + '/subdir';
 var firstTestFile = '/a',
 	secondTestFile = '/subdir/b';
 
-before(function() {
-	clearUp();
-	fs.mkdirsSync(subdir);
-	fs.writeFileSync(srcdir + firstTestFile, 'Test Data');
-	fs.utimesSync(srcdir + firstTestFile, atime, mtime);
-	fs.writeFileSync(srcdir + secondTestFile, 'Test Data');
-	fs.utimesSync(srcdir + secondTestFile, atime, mtime);
-});
-
 beforeEach(function() {
-	clearOutput();
+	clearUp();
+	fs.mkdirsSync(subdir, parseInt('777', 8));
+	fs.writeFileSync(srcdir + firstTestFile, 'Test Data', {
+		mode: parseInt('777', 8)
+	});
+	fs.utimesSync(srcdir + firstTestFile, atime, mtime);
+	fs.writeFileSync(srcdir + secondTestFile, 'Test Data', {
+		mode: parseInt('777', 8)
+	});
+	fs.utimesSync(srcdir + secondTestFile, atime, mtime);
 })
 
 after(function() {
@@ -32,47 +32,69 @@ after(function() {
 })
 
 it('should preserve file mtime', function(cb) {
-	
+
 	gulp.src(srcdir + '/**/*')
 		.pipe(gulp.dest(dest))
 		.pipe(preservetime())
 		.pipe(es.wait(function(err, data) {
-			
+
 			// Read file modification times in dest
 			var firstFileStats = fs.statSync(dest + firstTestFile),
 				secondFileStats = fs.statSync(dest + secondTestFile);
-			
+
 			firstFileStats.mtime.getTime().should.equal(mtime.getTime());
 			secondFileStats.mtime.getTime().should.equal(mtime.getTime());
-			
+
 			cb();
 		}));
 });
 
 it('should preserve file atime', function(cb) {
-	
+
 	gulp.src(srcdir + '/**/*')
 		.pipe(gulp.dest(dest))
 		.pipe(preservetime())
 		.pipe(es.wait(function(err, data) {
-			
+
 			// Read file modification times in dest
 			var firstFileStats = fs.statSync(dest + firstTestFile),
 				secondFileStats = fs.statSync(dest + secondTestFile);
-			
+
 			firstFileStats.atime.getTime().should.equal(atime.getTime());
 			secondFileStats.atime.getTime().should.equal(atime.getTime());
-			
+
+			cb();
+		}));
+});
+
+it('should not alter file', function(cb) {
+
+	gulp.src(srcdir + '/**/*')
+		.pipe(gulp.dest(dest))
+		.pipe(preservetime())
+		.pipe(es.wait(function(err, data) {
+
+			// Read file modification times in dest
+			var firstFileContent = fs.readFileSync(dest + firstTestFile, {
+				encoding: 'utf8'
+			}),
+			secondFileContent = fs.readFileSync(dest + secondTestFile, {
+				encoding: 'utf8'
+			});
+
+			firstFileContent.should.equal('Test Data');
+			secondFileContent.should.equal('Test Data');
+
 			cb();
 		}));
 });
 
 
 function clearUp() {
-	if (fs.existsSync(srcdir)) { 
+	if (fs.existsSync(srcdir)) {
 		fs.removeSync(srcdir);
 	}
-	clearOutput(); 
+	clearOutput();
 }
 
 function clearOutput() {

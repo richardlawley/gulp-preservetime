9d5af5e12913fd1dca1250eaadd7580c2df11e4f
Alexey Maryin <gz@groundzero.ru>
2016-10-12 06:03:19 +0300

    Call a stream-map callback to enable preservetime respond file-buffer to pipe stream.

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ var fs = require('graceful-fs');
 var map = require('map-stream');
 
 module.exports = function(options) {
-	return map(function(file, cb) {
-		if (file.isNull()) { return cb(null, file); }
+  return map(function(file, cb) {
+    if (file.isNull()) { return cb(null, file); }
 
-		// opens file
-       fs.open(file.path, 'a', function(err, fd) {
-           if (err) {
-               cb(err, file);
-               return;
-           }
+    // opens file
+    fs.open(file.path, 'a', function(err, fd) {
+      if (err) {
+          cb(err, file);
+          return;
+      }
 
-           // Update file modification and access time
-           fs.futimes(fd, file.stat.atime, file.stat.mtime, function(err) {
-                if (err) {
-                    cb(err, file);
-                    return;
-                }
+      if(file.stat.atime && file.stat.mtime){
+        // Update file modification and access time
+        fs.futimes(fd, file.stat.atime, file.stat.mtime, function(err) {
+          if (err) {
+              cb(err, file);
+              return;
+          }
 
-                fs.close(fd, cb);
-           });
-       });
-	});
+          fs.close(fd, cb);
+        });
+      }
+    });
+
+    cb(null, file);
+  });
 }

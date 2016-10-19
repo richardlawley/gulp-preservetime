0e4b50faa68009c35d19a47cc3efb06d615c9f4f
Richard Lawley <richard@richardlawley.com>
2016-10-19 14:08:17 +0100

    Fix whitespace

--- index.js
+++ index.js
@@ -3,26 +3,26 @@
 var fs = require('graceful-fs');
 var map = require('map-stream');
 
-module.exports = function(options) {
-	return map(function(file, cb) {
-		if (file.isNull()) { return cb(null, file); }
+module.exports = function (options) {
+    return map(function (file, cb) {
+        if (file.isNull()) { return cb(null, file); }
 
-		// opens file
-       fs.open(file.path, 'a', function(err, fd) {
-           if (err) {
-               cb(err, file);
-               return;
-           }
+        // opens file
+        fs.open(file.path, 'a', function (err, fd) {
+            if (err) {
+                cb(err, file);
+                return;
+            }
 
-           // Update file modification and access time
-           fs.futimes(fd, file.stat.atime, file.stat.mtime, function(err) {
+            // Update file modification and access time
+            fs.futimes(fd, file.stat.atime, file.stat.mtime, function (err) {
                 if (err) {
                     cb(err, file);
                     return;
                 }
 
                 fs.close(fd, cb);
-           });
-       });
-	});
+            });
+        });
+    });
 }
